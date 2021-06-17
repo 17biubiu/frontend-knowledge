b76ca8ae0adb6379ae2b6e81c93269fb32eead9c
17biubiu <nnhk001@163.com>
2021-06-17 15:19:41 +0800

    feat: config

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,5 +1,17 @@
 module.exports = {
   base: '/frontend-knowledge/',
   title: '工程师日常记录学习',
-  description: 'Just record.'
+  description: 'Just record.',
+  themeConfig: {
+    nav: [
+      {
+        text: '首页',
+        link: '/index/'
+      },
+      {
+        text: '技术',
+        link: '/tech/'
+      }
+    ]
+  }
 }
