84425a08421e6e4308e264b400b5c7152bc154ec
17biubiu <nnhk001@163.com>
2021-06-17 16:35:18 +0800

    feat: 修改文档

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -3,15 +3,20 @@ module.exports = {
   title: '工程师日常记录学习',
   description: 'Just record.',
   themeConfig: {
-    nav: [
-      {
-        text: '首页',
-        link: '/index/'
-      },
-      {
-        text: '技术',
-        link: '/tech/'
-      }
-    ]
+    navbar: true,
+    // 顶部导航栏内容
+    nav: [{ text: '指南', link: '/guide/' }],
+    // 侧边栏导航内容
+    sidebar: {
+      '/guide/': [
+        {
+          text: '指南',
+          children: [
+            { text: '介绍', link: '/guide/' },
+            { text: '快速上手', link: '/guide/getting-started' },
+          ],
+        },
+      ],
+    },
   }
 }
